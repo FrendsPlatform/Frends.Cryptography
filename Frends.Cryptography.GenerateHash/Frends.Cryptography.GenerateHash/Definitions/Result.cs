a6f7613b098415c771e32fb72d4664e0368a452d
Teemu Tossavainen <teemu.tossavainen@hiq.fi>
2022-08-11 09:13:31 +0300

    init

---
+++ Frends.Cryptography.GenerateHash/Frends.Cryptography.GenerateHash/Definitions/Result.cs
@@ -0,0 +1,13 @@
+﻿namespace Frends.Cryptography.GenerateHash.Definitions;
+
+/// <summary>
+/// Return HashResult.
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// HashResult.
+    /// </summary>
+    /// <example>8843d7f92416611de9ebb963fy4ce281v5932878</example>
+    public string Hash { get; set; }
+}
